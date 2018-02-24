44468139ae1feec7df9e4710d0d7bf572da4a171
Buss, Marco <marco.buss.81@googlemail.com>
2018-02-24 21:43:29 +0100

    add new policy for developer to deploy own stack

--- build/deploy_master_stack.sh
+++ build/deploy_master_stack.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+aws cloudformation deploy --template-file template.yml --stack-name dev-oc-papersubmission-angular-ui --parameter-override GithubToken="${GITHUB_TOKEN}" UIBucket="${UI_BUCKET}" --capabilities CAPABILITY_NAMED_IAM
