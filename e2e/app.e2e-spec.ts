cc337784cfb0e3fae7d4b7a12dc41039392619d0
Buss, Marco <marco.buss.81@googlemail.com>
2018-02-24 22:06:56 +0100

    regenerate app with actual angular-cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { PapersubmissionUiPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('papersubmission-ui App', function() {
-  let page: PapersubmissionUiPage;
+describe('papersubmission-ui App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new PapersubmissionUiPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
